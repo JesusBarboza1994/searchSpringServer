4aed4a77434ac68d36b6089353f9708b181f777b
Jesus Barboza <jbarz20@gmail.com>
2023-08-17 11:29:23 -0500

    .

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module SearchSpringServer
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.bootsnap = { disable: true }
+    # config.bootsnap = { disable: true }
   end
 end
